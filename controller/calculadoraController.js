0d9bd6b797f28c7204ed4e73785270b54a1e63f6
Hélen Gabriele <helengabrieleveleda123@gmail.com>
2021-08-19 11:00:04 -0300

    controller

--- controller/calculadoraController.js
+++ controller/calculadoraController.js
@@ -1,65 +1,22 @@
-var Usuario = require("../model/Usuario");
+async function
+ abreCalculadora(req, res) {  res.render("calculadora.ejs", {});}
 
-async function abreAdd(req, res) {
-  res.render("usuario/add.ejs", { msg: "" });
-}
+async function calcula(req, res) { 
+   var num1 = req.body.num1;  
+   var num2 = req.body.num2;  
+   var operacao = req.body.operacao; 
+    var result;
+  if (operacao == "+")
+ {    result = parseInt(num1) + parseInt(num2); 
+      } else if (operacao == "-")
+       {    result = parseInt(num1) - parseInt(num2); 
+       } else if (operacao == "/")
+        {    result = parseInt(num1) / parseInt(num2); 
+         } else if (operacao == "*")
+          {    result = parseInt(num1) * parseInt(num2); 
+           }
+  res.render("resultado.ejs",
+ {    resultado: result,    operacao: operacao,    num1: num1,    num2: num2,  });}
 
-async function add(req, res) {
-  var nome = req.body.nome;
-  var email = req.body.email;
-  var senha = req.body.senha;
-  var foto = req.body.foto;
-
-  var novoUsuario = new Usuario({
-    nome: nome,
-    email: email,
-    senha: senha,
-    foto: foto,
-  });
-
-  novoUsuario.save(function (err) {
-    if (err) {
-      console.log("O erro que aconteceu foi: " + err);
-    } else {
-      res.render("usuario/add.ejs", { msg: "Usuário adicionado!" });
-    }
-  });
-}
-
-async function listar(req, res) {
-  Usuario.find({})
-    .lean()
-    .exec(function (err, docs) {
-      if (err) {
-        console.log("O erro que aconteceu foi: " + err);
-      } else {
-        res.render("usuario/list.ejs", { Usuarios: docs });
-      }
-    });
-}
-
-async function listarFiltro(req, res) {}
-
-async function abreEdt(req, res) {}
-
-async function edt(req, res) {}
-
-async function del(req, res) {
-  Usuario.findByIdAndDelete(req.params.id).exec(function (err) {
-    if (err) {
-      console.log("O erro que aconteceu foi: " + err);
-    } else {
-      res.redirect("/usuario");
-    }
-  });
-}
-
-module.exports = {
-  add,
-  abreAdd,
-  listar,
-  listarFiltro,
-  abreEdt,
-  edt,
-  del,
-};
+ 
+module.exports = { abreCalculadora, calcula };
--- routes/calculadoraRoute.js
+++ routes/calculadoraRoute.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const router = express.Router();
-var usuarioController = require("../controller/calculadoraController");
+var calculadoraController = require("../controller/calculadoraController");
 
-//CREATE
-router.get("/add", usuarioController.abreAdd);
-router.post("/add", usuarioController.add);
+//
+router.post("/", calculadoraController.abreCalculadora);
 
-//READ
-router.get("/", usuarioController.listar);
-router.post("/", usuarioController.listarFiltro);
+//
+router.post("/", calculadoraController.calcula);
 
-//UPDATE
-router.get("/edt/:id", usuarioController.abreEdt);
-router.post("/edt/:id", usuarioController.edt);
-
-//DELETE
-router.get("/del/:id", usuarioController.del);
 
 module.exports = router;
